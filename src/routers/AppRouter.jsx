4c651fd342b6dba772a84fb074ab1532cd4637e2
aalesann <alejandrosanabria74@gmail.com>
2022-11-04 01:54:10 -0300

    First commit 4-11-2022

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { PublicRoutes } from "./PublicRoutes"
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/login' element={ 
+          <PublicRoutes>
+            {/* Colocar el componente a proteger */}
+          </PublicRoutes>
+        } />
+
+        <Route path='/*' element={
+          // Proteger las rutas privadas
+        } />
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
